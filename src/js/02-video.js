0ea123cd323461e39970040bf8b49c80493eb6c8
Streltsov Valerii <st.valera04@gmail.com>
2023-02-21 20:40:12 +0200

    Done second and third tascks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Players from '@vimeo/player';
+var _ = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Players(iframe);
 
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    if (seconds > 1) {
+      alert(`Ви зупинились на ${seconds} секунді`);
+    }
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on('timeupdate', _(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+var _ = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+function fillingOutFormFromLocalStorage() {
+  let parsedValues;
+  try {
+    parsedValues = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(parsedValues);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+
+  if (parsedValues !== null) {
+    if (parsedValues.email != '' || parsedValues.message != '') {
+      feedbackForm.email.value = parsedValues.email;
+      feedbackForm.message.value = parsedValues.message;
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', fillingOutFormFromLocalStorage);
+
+function saveCuretnValuesOfForm() {
+  const currentValues = {
+    email: feedbackForm.email.value,
+    message: feedbackForm.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentValues));
+}
+
+feedbackForm.addEventListener('input', _(saveCuretnValuesOfForm, 500));
+
+function submitForm(e) {
+  e.preventDefault();
+
+  console.log(feedbackForm.email.value);
+  console.log(feedbackForm.message.value);
+
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.email.value = '';
+  feedbackForm.message.value = '';
+}
+
+feedbackForm.addEventListener('submit', submitForm);
